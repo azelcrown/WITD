590fab9806cf2821e72990f98ed5744be8333f9d
vitanex <mariamsoler00@gmail.com>
2024-05-07 14:47:36 +0200

    sonido y movimiento

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+public AudioClip fxMapa; 
+public AudioClip fxBichorl; 
+public AudioClip fxBichorlCute; 
+public AudioClip fxBichorlEww; 
+public AudioClip fxBichorlgogo; 
+public AudioClip fxBichorltontopolla; 
+public static AudioManager Instance;
+public GameObject musicObj;
+AudioSource _audioSource;
+AudioSource audioMusic;
+
+void Awake(){
+
+    if(Instance != null && Instance != this){
+        
+        Destroy(this.gameObject);
+
+    }else{
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         _audioSource = this.GetComponent<AudioSource>();
+
+        audioMusic = musicObj.GetComponent<AudioSource>();
+        audioMusic.clip = fxMapa;
+        audioMusic.loop = true;
+        audioMusic.volume = 0.2f;
+        audioMusic.Play();
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
