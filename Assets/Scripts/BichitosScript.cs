5e208d4fe57e8f54b9285c12ae2dd356af21d89c
vitanex <mariamsoler00@gmail.com>
2024-05-15 14:35:40 +0200

    Call and response funcional!!!!!

--- Assets/Scripts/BichitosScript.cs
+++ Assets/Scripts/BichitosScript.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class BichitosScript : MonoBehaviour
 {
-    GameObject fpc;
+   
     public AudioClip bichitosResponse;
 
     private AudioSource _audioSource;
 
 
+
     // Start is called before the first frame update
     void Start()
     {   
-        fpc = GameObject.Find("FirstPersonController");
+
         _audioSource = GetComponent<AudioSource>();
     }
 
-    
-    public void EnteredPlayerTrigger(){
+     public void PlayResponseSound(float volume){
+
+        _audioSource.volume = volume;
         _audioSource.PlayOneShot(bichitosResponse);
+    }
+    public void EnteredPlayerTrigger(Transform playerTransform, float maxDistance){
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
+        float volume = Mathf.Clamp01(1 - distance / maxDistance);
+        PlayResponseSound(volume);
         
     }
 
--- Assets/Scripts/EmisionSonidos.cs
+++ Assets/Scripts/EmisionSonidos.cs
@@ -7,9 +7,13 @@ public class EmisionSonidos : MonoBehaviour
 {
 
     public AudioClip playerCall;
+    public Transform playerTransform;
+    public float maxDistance = 25f; //Distancia máximo para que el sonido de los bichos suene a tope
     private AudioSource playerAudioSource;
 
     private bool llamadaHecha = false;
+    private bool permitirLlamada = true;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +24,12 @@ public class EmisionSonidos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Z)){
+        if(permitirLlamada && Input.GetKeyDown(KeyCode.Z) && !playerAudioSource.isPlaying){
             playerAudioSource.PlayOneShot(playerCall);
+            permitirLlamada = false;
+            
+        }else{
+            permitirLlamada = true;
         }
     }
 
@@ -31,34 +39,44 @@ public class EmisionSonidos : MonoBehaviour
 
         if(col.gameObject.CompareTag("Bicho") && playerAudioSource.isPlaying){
            
-            
-            if(llamadaHecha == false){
-                StartCoroutine(Respuesta(col));
-                llamadaHecha = true;
-            }
+           BichitosScript bichitos = col.GetComponent<BichitosScript>();
+            if (bichitos != null)
+            {
+                if (!llamadaHecha)
+                {
+                    StartCoroutine(Respuesta(bichitos));
+                    llamadaHecha = true;
+                }
             Debug.Log("Gritan los bichoss");
+            }
         }
     } 
 
     void OnTriggerExit(Collider col){
 
-        if(col.gameObject.CompareTag("Bicho"))
-        {   BichitosScript bichitos = col.GetComponent<BichitosScript>();
-             bichitos.ExitedPlayerTrigger();
-            Debug.Log("No Gritan los bichoss");
+        if(col.gameObject.CompareTag("Bicho")){
+
+            
+            BichitosScript bichitos = col.GetComponent<BichitosScript>();
+            bichitos.ExitedPlayerTrigger();
+            Debug.Log("No Gritan los bichos");
         }
-    } 
+    }
 
+    IEnumerator Respuesta(BichitosScript bichitos){
 
-    IEnumerator Respuesta(Collider col){
-
-        yield return new WaitForSeconds(1.5f);
-        BichitosScript bichitos = col.GetComponent<BichitosScript>();
-        bichitos.EnteredPlayerTrigger();
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(0.5f);
+        //distancia entre jugador y bichos
+        float distance = Vector3.Distance(transform.position, bichitos.transform.position);
+        //volumen en función de distancia, si estoy dentro del trigger
+       float volume =Mathf.Clamp01(1 - distance / maxDistance);
+       bichitos.EnteredPlayerTrigger(transform,maxDistance);
+        yield return new WaitForSeconds(2f);
         llamadaHecha = false;
+        permitirLlamada = true;
         Debug.Log("Puedo pulsar otra vez");
 
     }
+    
 }
 
